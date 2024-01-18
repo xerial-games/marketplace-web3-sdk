ef39f4e94449c74c13e3995fb06e1a396cb7fd71
Richard Irala <richardirala47@hotmail.com>
2024-01-18 11:57:34 -0300

    Fix: InventoryItemOnSecondaryMarket atom UI

--- atoms/InventoryItemOnSecondaryMarket/InventoryItemOnSecondaryMarket.jsx
+++ atoms/InventoryItemOnSecondaryMarket/InventoryItemOnSecondaryMarket.jsx
@@ -8,10 +8,11 @@ const InventoryItemOnSecondaryMarket = ({ nft }) => {
   return (
     <div className="inventory-items__container">
       <div className="inventory-items__projectContainer">
-        <h1>Nft typeId: {nft.typeId}</h1>
-        <h2>Nft name: {nft.metadata.name}</h2>
         <img src={nft.metadata.image} alt={nft.metadata.name} className="inventory-items__image"/>
+        <h1>Nft marketItemId: {nft.marketItemId}</h1>
+        <h2>Nft name: {nft.metadata.name}</h2>
         <p>collectionAddress: {nft.metadata.contract.address}</p>
+        <p>collectionName: {nft.metadata.contract.name}</p>
         <p className="atom-item__generalText atom-item__priceMagenta">Price: {nft.price} USDC</p>
       </div>
       <button className="inventory-items__buttonSell" type="submit" onClick={onDelistNft}>
