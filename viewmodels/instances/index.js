8c751603fd46c1e032504cebff1e9d1c13816dd3
Richard Irala <richardirala47@hotmail.com>
2024-01-30 11:48:12 -0300

    Fix: Xerial wallet functions

--- viewmodels/instances/index.js
+++ viewmodels/instances/index.js
@@ -1,6 +1,6 @@
 import { DomHelper } from "@/helpers/dom";
 import { ObserverHelper } from "@/helpers/observer";
-import web2Functions from "@/utils/web2_functions/web2_functions";
+import web2Functions from "@/functions/web2/web2";
 import XerialWalletViewmodel from "../xerial_wallet";
 
 export const xerialWalletViewmodelInstance = new XerialWalletViewmodel({
