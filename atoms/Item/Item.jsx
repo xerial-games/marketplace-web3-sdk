b01128d4b0ede9a1ccc1a0e11e0cca2a6b33e73f
Richard Irala <richardirala47@hotmail.com>
2024-02-22 14:56:02 -0300

    Feature: added handle balance error in purchaseNft functions. The error is now being rendered on screen

--- atoms/Item/Item.jsx
+++ atoms/Item/Item.jsx
@@ -1,33 +1,65 @@
 import web3Functions from "@/functions/web3/web3";
+import capitalizeFirstLetter from "@/utils/capitalizeFirstLetter";
 import { useEffect, useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 const Item = ({ nft, sellerAddress, XerialWalletViewmodel, activeChain }) => {
   const [amount, setAmount] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handlePurchaseError(error) {
+    const ownerCallDelistRegex =
+      /execution reverted: Err: Owner, please call delist/;
+    const balanceErrorRegex = /transfer amount exceeds balance/;
+    const ownerCallDelistMatchResponse =
+      error.message.match(ownerCallDelistRegex);
+    const balanceMatchResponse = error.message.match(balanceErrorRegex);
+    if (ownerCallDelistMatchResponse) {
+      const specificMessage = ownerCallDelistMatchResponse[0];
+      console.error(specificMessage);
+      setErrorMessage(capitalizeFirstLetter(specificMessage));
+    }
+  
+    if (balanceMatchResponse) {
+      const specificMessage = balanceMatchResponse[0];
+      console.error(specificMessage);
+      setErrorMessage(capitalizeFirstLetter(specificMessage));
+    }
+  }
+
+  async function purchaseNfts(event) {
+    event.preventDefault();
+    try {
+      if (!amount || amount <= 0) {
+        console.error("Please Set a Valid Amount");
+        return;
+      }
+      if (XerialWalletViewmodel.loguedWith === "google") {
+        await XerialWalletViewmodel.purchaseNfts({
+          tokenTypeId: nft.typeId,
+          quantity: amount,
+          collectionAddress: nft.metadata.contract.address,
+        });
+      } else {
+        await web3Functions.purchaseNft({
+          tokenTypeId: nft.typeId,
+          collectionAddress: nft.metadata.contract.address,
+          quantity: amount,
+          chain: activeChain,
+        });
+      }
+    } catch (error) {
+      handlePurchaseError(error);
+      console.error("Error to purchase NFTs. Reason: " + error.message);
+    }
+  }
+
+  function onCloseErrorMessage () {
+    setErrorMessage("");
+  }
+
   return (
-    <form
-      className="atom-item__box"
-      onSubmit={async (event) => {
-        event.preventDefault();
-        if (!amount || amount <= 0) {
-          console.error("Please Set a Valid Amount");
-          return;
-        }
-        if (XerialWalletViewmodel.loguedWith === "google") {
-          await XerialWalletViewmodel.purchaseNfts({
-            tokenTypeId: nft.typeId,
-            quantity: amount,
-            collectionAddress: nft.metadata.contract.address,
-          });
-        } else {
-          await web3Functions.purchaseNft({
-            tokenTypeId: nft.typeId,
-            collectionAddress: nft.metadata.contract.address,
-            quantity: amount,
-            chain: activeChain
-          });
-        }
-      }}
-    >
+    <form className="atom-item__box" onSubmit={purchaseNfts}>
       <img className="atom-item__itemImage" src={nft.metadata.image} alt="nft img" />
       <div className="atom-item__priceAndTypeIdContainer">
         <h1 className="atom-item__generalText global-style__textWithDots">Token typeId: {nft.typeId}</h1>
@@ -61,6 +93,11 @@ const Item = ({ nft, sellerAddress, XerialWalletViewmodel, activeChain }) => {
         />
         <button className="atom-item__button">Buy</button>
       </div>
+      <ErrorMessage
+        error={errorMessage}
+        isOpen={errorMessage}
+        onClose={onCloseErrorMessage}
+      />
     </form>
   );
 };
